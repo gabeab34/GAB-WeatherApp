f279d5899b421cf50e812fd4f7f294e676c3504c
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-06-07 16:49:15 -0400

    added arrow functions to display values from api call

---
+++ weatherscript.js
@@ -0,0 +1,68 @@
+
+var api = "d4c9adabf7b5976ac4b9218b7c3b9b66";
+var city = "";
+var currentcity         = $("#current-city");
+var currenttemperature  = $("#temperature");
+var currenthumidity     = $("#humidity");
+var currentwindspeed    = $("#wind-speed");
+var currentUVindex      = $("#uv-index");
+var searchcity          = $("#search-city");
+
+
+
+const displayweather = (event) => {
+    event.preventDefault();
+    if(searchcity.val().trim()!==""){
+        city=searchcity.val().trim();
+        currentweather(city);
+    }
+}
+
+const currentweather = (city) => {
+    var apiquery= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api;
+    $.ajax({
+        url:apiquery,
+        method:"get",
+    }).then((response) => {
+
+        
+        console.log(response);
+        
+        var today=moment(response)
+       
+        $(currentcity).html(response.name +"("+today.format("dddd, MMMM Do")+")");
+       
+        var temperature = (response.main.temp)
+        
+        $(currenttemperature).html(temperature);
+        
+        $(currenthumidity).html(response.main.humidity);
+        
+        var ws=response.wind.speed;
+        
+        var wind=(ws)
+        
+        $(currentwindspeed).html(wind);
+        
+        UVIndex(response.coord.lon,response.coord.lat);
+        
+
+    });
+}
+    
+const UVIndex = (ln,lt) => {
+    var UVquery="https://api.openweathermap.org/data/2.5/uvi?appid="+ api +"&lat="+lt+"&lon="+ln;
+    $.ajax({
+        url:UVquery,
+        method:"get"
+        }).then((response) => {
+            $(currentUVindex).html(response.value);
+        });
+}
+    
+
+
+
+
+
+$("#search-button").on("click",displayweather);
