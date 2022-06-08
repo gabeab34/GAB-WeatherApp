3410a866b500acedbb91aa9ea28cb1c4c67af47a
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-06-08 18:31:33 -0400

    added arrow function for five day forcast

--- weatherscript.js
+++ weatherscript.js
@@ -9,7 +9,6 @@ var currentUVindex      = $("#uv-index");
 var searchcity          = $("#search-city");
 
 
-
 const displayweather = (event) => {
     event.preventDefault();
     if(searchcity.val().trim()!==""){
@@ -19,8 +18,7 @@ const displayweather = (event) => {
 }
 
 const currentweather = (city) => {
-    var apiquery= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api + "&units=imperial"
-    ; 
+    var apiquery= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + api + "&units=imperial"; 
 
     $.ajax({
         url:apiquery,
@@ -31,8 +29,12 @@ const currentweather = (city) => {
         console.log(response);
         
         var today=moment(response)
-       
-        $(currentcity).html(response.name +"("+today.format("dddd, MMMM Do")+")");
+        
+        var icon = response.weather[0].icon
+
+        var iconsrc =  "https://openweathermap.org/img/wn/"+icon +"@2x.png";
+
+        $(currentcity).html(response.name + "<br></br>" + ""+today.format("dddd, MMMM Do")  + "<img src=" +iconsrc+">");
 
         // .current.weather[0].icon https://openweathermap.org/img/wn/${icon}@2x.png"
         // .daily[0].weather[0].icon
@@ -55,6 +57,11 @@ const currentweather = (city) => {
         UVIndex(response.coord.lon,response.coord.lat);
         
 
+       
+
+
+        
+
     });
 }
     
@@ -65,12 +72,48 @@ const UVIndex = (ln,lt) => {
         method:"get"
         }).then((response) => {
             $(currentUVindex).html(response.value);
+            fiveday(response.id)
         });
-}
+        
+
+
+    const fiveday = () => {
+            var fivedayquery="https://api.openweathermap.org/data/2.5/forecast?lat="+lt+"&lon="+ln+"&appid="+api + "&units=imperial";
+            $.ajax({
+                url:fivedayquery,
+                method:"get"
+            }).then((response) => {
+                
+                for (i=0;i<5;i++){
+                    var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+                    var iconcode= response.list[((i+1)*8)-1].weather[0].icon;
+                    var iconurl="https://openweathermap.org/img/wn/"+iconcode+".png";
+                    var temperature= (response.list[((i+1)*8)-1].main.temp);
+                    var humidity= response.list[((i+1)*8)-1].main.humidity;
+                
+                    $("#fiveday"+i).html(date);
+                    $("#fiveday"+i).append("<img src="+iconurl+">");
+                    $("#fiveday"+i).append(temperature);
+                    $("#fiveday"+i).append( "<br></br>" + humidity+"%");
+                }
+                
+            });
+        }
+        
     
+    
+    
+    
+    
+    
+    
+    
+    
+}
 
 
 
 
 
 $("#search-button").on("click",displayweather);
+
